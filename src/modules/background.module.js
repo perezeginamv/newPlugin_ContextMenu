07c8f9962b10a89acbd50a0a5418c6c996422fc6
Marina <perezeginamv@mail.ru>
2022-11-19 21:04:26 +0500

    added module change background color

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,37 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
+import { random } from "../utils";
 
 export class BackgroundModule extends Module {
+  constructor() {
+    super("Background", "Поменять цвет фона");
+  }
 
+  trigger() {
+    const arrayOfColorDigits = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+    const colorSelectionPreparation = ["#"];
+
+    for (let i = 1; i < 7; i++) {
+      let randomSelection = random(0, (arrayOfColorDigits.length - 1));
+      colorSelectionPreparation.push(arrayOfColorDigits[randomSelection]);
+    }
+    const chooseСolor = colorSelectionPreparation.join("");
+    document.body.style.background = chooseСolor;
+  }
 }
